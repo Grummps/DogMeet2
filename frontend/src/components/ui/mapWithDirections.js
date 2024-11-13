f2b4d633a6eaf3b7518587817d2048bcc65082bb
Grummps <leitetravis@gmail.com>
2024-11-13 18:02:12 -0500

    Removed comment

--- frontend/src/components/ui/mapWithDirections.js
+++ frontend/src/components/ui/mapWithDirections.js
@@ -1,4 +1,3 @@
-// src/components/MapWithDirections.jsx
 import React, { useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from 'react-leaflet';
 import L from 'leaflet';
